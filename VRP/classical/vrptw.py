baee4e73f75491ed91c6eb58cedba96f4e64ed41
Katarzyna Nałęcz-Charkiewicz <k.m.nalecz@gmail.com>
2022-09-19 11:27:11 +0200

    Implementation of VRPTW using Google OR-Tools

--- VRP/classical/vrptw.py
+++ VRP/classical/vrptw.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
 """
 Solving VRPTW problem using Google OR-Tools.
 """
@@ -27,5 +31,126 @@ class VRPTW:
         self.yc = yc  # matrix of y coordinates for each city
         self.tw = tw  # list of time windows (for each city)
 
+    def __prepare_data(self):
+        data = {}
+        data["distance_matrix"] = self.dist.tolist()
+        data["time_matrix"] = self.time.tolist()
+        data["time_windows"] = self.tw.tolist()
+        data["num_vehicles"] = self.m
+        data["depot"] = 0
+
+        return data
+
+    def __print_solution(self, manager, routing, solution):
+        """Prints solution on console."""
+        print(f"Objective: {solution.ObjectiveValue()}")
+        time_dimension = routing.GetDimensionOrDie("Time")
+        total_distance = 0
+        total_time = 0
+        for vehicle_id in range(manager.GetNumberOfVehicles()):
+            index = routing.Start(vehicle_id)
+            plan_output = "Route for vehicle {}:\n".format(vehicle_id)
+            distance = 0
+            while not routing.IsEnd(index):
+                time_var = time_dimension.CumulVar(index)
+                plan_output += "{0} Time({1},{2}) -> ".format(
+                    manager.IndexToNode(index),
+                    solution.Min(time_var),
+                    solution.Max(time_var),
+                )
+
+                previous_index = index
+                index = solution.Value(routing.NextVar(index))
+                distance += routing.GetArcCostForVehicle(
+                    previous_index, index, vehicle_id
+                )
+            time_var = time_dimension.CumulVar(index)
+            plan_output += "{0} Time({1},{2})\n".format(
+                manager.IndexToNode(index),
+                solution.Min(time_var),
+                solution.Max(time_var),
+            )
+            plan_output += "Distance of the route: {0}m\n".format(distance)
+            plan_output += "Time of the route: {}min\n".format(solution.Min(time_var))
+            print(plan_output)
+            total_distance += distance
+            total_time += solution.Min(time_var)
+        print("Total Distance of all routes: {0}m".format(total_distance))
+        print("Total time of all routes: {}min".format(total_time))
+
     def solve(self):
+        data = self.__prepare_data()
+        manager = pywrapcp.RoutingIndexManager(
+            len(data["time_matrix"]), data["num_vehicles"], data["depot"]
+        )
+        routing = pywrapcp.RoutingModel(manager)
+
+        def distance_callback(from_index, to_index):
+            """Returns the distance between the two nodes."""
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return data["distance_matrix"][from_node][to_node]
+
+        distance_evaluator_index = routing.RegisterTransitCallback(distance_callback)
+
+        routing.SetArcCostEvaluatorOfAllVehicles(distance_evaluator_index)
+
+        def time_callback(from_index, to_index):
+            """Returns the travel time between the two nodes."""
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return data["time_matrix"][from_node][to_node]
+
+        transit_callback_index = routing.RegisterTransitCallback(time_callback)
+
+        # time windows constraint
+        time = "Time"
+        routing.AddDimension(
+            transit_callback_index,
+            30,  # allow waiting time
+            30,  # maximum time per vehicle
+            False,  # Don't force start cumul to zero.
+            time,
+        )
+        time_dimension = routing.GetDimensionOrDie(time)
+
+        for location_idx, time_window in enumerate(data["time_windows"]):
+            if location_idx == data["depot"]:
+                continue
+            index = manager.NodeToIndex(location_idx)
+            time_dimension.CumulVar(index).SetRange(time_window[0], time_window[1])
+
+        depot_idx = data["depot"]
+        for vehicle_id in range(data["num_vehicles"]):
+            index = routing.Start(vehicle_id)
+            time_dimension.CumulVar(index).SetRange(
+                data["time_windows"][depot_idx][0], data["time_windows"][depot_idx][1]
+            )
+
+        for i in range(data["num_vehicles"]):
+            routing.AddVariableMinimizedByFinalizer(
+                time_dimension.CumulVar(routing.Start(i))
+            )
+            routing.AddVariableMinimizedByFinalizer(
+                time_dimension.CumulVar(routing.End(i))
+            )
+
+        # setting solution heuristic
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+        )
+
+        solution = routing.SolveWithParameters(search_parameters)
+
+        # Print solution on console.
+        if solution:
+            self.__print_solution(manager, routing, solution)
+
+            return dict({"min_cost": solution.ObjectiveValue()})
+
+        print("No solution found!")
+
+    def visualize(self):
+        """Visualize solution"""
         pass
--- VRP/vrptw_experiments.py
+++ VRP/vrptw_experiments.py
@@ -8,10 +8,10 @@ class Initializer:
         self.a = a
         self.b = b
 
-        self.MIN_TIME_WINDOW = 5    # lower bound for time window
-        self.MAX_TIME_WINDOW = 30   # upper bound for time window
+        self.MIN_TIME_WINDOW = 5  # lower bound for time window
+        self.MAX_TIME_WINDOW = 30  # upper bound for time window
 
-        self.MIN_SERVICE_TIME = 1   # lower bound for service time
+        self.MIN_SERVICE_TIME = 1  # lower bound for service time
         self.MAX_SERVICE_TIME = 20  # upper bound for service time
 
     def generate_nodes_weight_matrix_time_windows(self):
@@ -35,14 +35,17 @@ class Initializer:
             earliest_time = round(np.random.rand() * self.MIN_TIME_WINDOW)
             latest_time = round(np.random.rand() * self.MAX_TIME_WINDOW + earliest_time)
             time_windows.append([earliest_time, latest_time])
-        
-        time_windows = np.insert(time_windows, 0, [0, 0], axis=0)  # depot time window
+        time_windows[0][0] = 0
+        # TODO check whether the following line will not be necessary
+        # in the case of implementation in accordance with the Adam and Ozlem draft
+        # time_windows = np.insert(time_windows, 0, [0, 0], axis=0)  # depot time window
+        time_windows = np.array(time_windows)
 
         travel_times = np.random.randint(
             self.MIN_SERVICE_TIME, self.MAX_SERVICE_TIME, size=(n, n)
         )
         np.fill_diagonal(travel_times, 0)
-        travel_times = ((travel_times + travel_times.T)/2).astype(int)
+        travel_times = ((travel_times + travel_times.T) / 2).astype(int)
 
         return x, y, weight_matrix, travel_times, time_windows
 
@@ -54,8 +57,6 @@ if __name__ == "__main__":
 
     initializer = Initializer(n + 1, n + 1, 0)
     xc, yc, dist, time, tw = initializer.generate_nodes_weight_matrix_time_windows()
-
-    tw = np.array([[0, 0], [0, 20], [3, 14], [1, 6], [5, 6]])
 
     # Solve VRPTW with classical solver:
     classical_solver = VRPTW(n, m, dist, xc=xc, yc=yc, tw=tw, time=time)
